c164546fd608144fe74f349e45d0a00ac49adb5d
Faiaz Rahman <faiazrahman70@gmail.com>
2022-01-14 14:41:49 +0600

    Validation Schema Added!

--- src/screens/CustomerRegistration.js
+++ src/screens/CustomerRegistration.js
@@ -10,32 +10,65 @@ import {
 } from 'react-native';
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import CalendarPicker from 'react-native-calendar-picker';
+import {Formik} from 'formik';
+import * as yup from 'yup';
 
 import CustomInput from '../components/CustomInput';
 
 import {COLORS, DIM} from '../constants';
+import Buttons from '../components/Buttons';
 
 export default function CustomerRegistration() {
-  const [email, setEmail] = useState('');
-  const [db, setDb] = useState('');
-  const [name, setName] = useState('');
-  const [pass, setPass] = useState('');
-  const [confPass, setConfPass] = useState('');
-  const [ph, setPh] = useState('');
-  const [address, setAddress] = useState('');
+  const [selectedDate, setSelectedDate] = useState('Date of Birth');
+
+  const validationSchema = yup.object().shape({
+    name: yup.string().required('Your name is required !'),
+    email: yup
+      .string()
+      .email('Please enter valid email')
+      .required('Email Address is required !'),
+    pass: yup.string().required('Password is required !'),
+    confPass: yup
+      .string()
+      .oneOf([yup.ref('pass'), null], 'Passwords not match !'),
+    phone: yup.string().required('Phone Number is required !'),
+    address: yup.string().required('Address is required !'),
+  });
 
   const [calOpen, setCalOpen] = useState(false);
-
-  const [selectedDate, setSelectedDate] = React.useState('2020-02-01');
-
   return (
     <>
       {calOpen && (
         <Modal
           animationType="slide"
           transparent
-          onRequestClose={() => setCalOpen(false)}>
-          <View style={styles.modalStyle}></View>
+          onRequestClose={() => {
+            setCalOpen(false);
+          }}>
+          <View style={styles.modalStyle}>
+            <CalendarPicker
+              onDateChange={date => {
+                const month = date.toString().slice(4, 7);
+                const datetime = date.toString().slice(8, 10);
+                const year = date.toString().slice(11, 15);
+                // console.log(month, datetime, year);
+                setSelectedDate(month + ' ' + datetime + ', ' + year);
+              }}
+              selectedDayStyle={{
+                backgroundColor: COLORS.primary,
+                color: COLORS.white,
+              }}
+              textStyle={{
+                fontSize: 15,
+                fontWeight: '600',
+                color: COLORS.black,
+              }}
+              scrollable
+              height={DIM.height * 0.55}
+              width={DIM.width * 0.87}
+            />
+          </View>
         </Modal>
       )}
 
@@ -59,61 +92,160 @@ export default function CustomerRegistration() {
           }}>
           Customer Registration
         </Text>
-        <CustomInput
-          text="Enter your name"
-          iconName="create-sharp"
-          customStyle={styles.custom}
-          onChangeText={text => setName(text)}
-        />
-
-        <TouchableWithoutFeedback
-          onPress={() => {
-            setCalOpen(true);
-          }}>
-          <View style={styles.textContainer}>
-            <MaterialCommunityIcons
-              name="calendar"
-              size={30}
-              color={'grey'}
-              style={styles.icon}
-            />
-            <Text style={styles.text}>Date of Birth</Text>
-          </View>
-        </TouchableWithoutFeedback>
-
-        <CustomInput
-          text="Enter the email"
-          iconName="mail"
-          customStyle={styles.custom}
-          onChangeText={text => setEmail(text)}
-        />
-        <CustomInput
-          text="Enter password"
-          iconName="lock-open"
-          customStyle={styles.custom}
-          onChangeText={text => setPass(text)}
-          passEntry
-        />
-        <CustomInput
-          text="Confirm password"
-          iconName="lock-open"
-          customStyle={styles.custom}
-          onChangeText={text => setConfPass(text)}
-          passEntry
-        />
-        <CustomInput
-          text="Enter phone no."
-          iconName="call"
-          customStyle={styles.custom}
-          onChangeText={text => setPh(text)}
-          keyboardType={'numeric'}
-        />
-        <CustomInput
-          text="Enter your Address"
-          iconName="location-sharp"
-          customStyle={styles.custom}
-          onChangeText={text => setAddress(text)}
-        />
+        <Formik
+          initialValues={{
+            name: '',
+            dob: '',
+            email: '',
+            pass: '',
+            confPass: '',
+            phone: '',
+            address: '',
+          }}
+          validationSchema={validationSchema}
+          onSubmit={values => console.log(values)}>
+          {({handleSubmit, handleChange, errors, values, touched}) => (
+            <>
+              <CustomInput
+                text="Enter your Name"
+                iconName="create-sharp"
+                customStyle={styles.custom}
+                onChangeText={handleChange('name')}
+              />
+              {errors.name && touched.name && (
+                <View>
+                  <Text
+                    style={{
+                      color: COLORS.primary,
+                      fontSize: 15,
+                      fontWeight: '600',
+                      paddingBottom: 10,
+                    }}>
+                    {errors.name}
+                  </Text>
+                </View>
+              )}
+              <TouchableWithoutFeedback
+                onPress={() => {
+                  setCalOpen(true);
+                }}>
+                <View style={styles.textContainer}>
+                  <MaterialCommunityIcons
+                    name="calendar"
+                    size={30}
+                    color={'grey'}
+                    style={styles.icon}
+                  />
+                  <Text style={styles.text}>{selectedDate}</Text>
+                </View>
+              </TouchableWithoutFeedback>
+              <CustomInput
+                text="Enter the Email"
+                iconName="mail"
+                customStyle={styles.custom}
+                onChangeText={handleChange('email')}
+              />
+              {errors.email && touched.email && (
+                <View>
+                  <Text
+                    style={{
+                      color: COLORS.primary,
+                      fontSize: 15,
+                      fontWeight: '600',
+                      paddingBottom: 10,
+                    }}>
+                    {errors.email}
+                  </Text>
+                </View>
+              )}
+              <CustomInput
+                text="Enter Password"
+                iconName="lock-open"
+                customStyle={styles.custom}
+                passEntry
+                onChangeText={handleChange('pass')}
+              />
+              {errors.pass && touched.pass && (
+                <View>
+                  <Text
+                    style={{
+                      color: COLORS.primary,
+                      fontSize: 15,
+                      fontWeight: '600',
+                      paddingBottom: 10,
+                    }}>
+                    {errors.pass}
+                  </Text>
+                </View>
+              )}
+              <CustomInput
+                text="Confirm Password"
+                iconName="lock-open"
+                customStyle={styles.custom}
+                passEntry
+                onChangeText={handleChange('confPass')}
+              />
+              {errors.confPass && touched.confPass && (
+                <View>
+                  <Text
+                    style={{
+                      color: COLORS.primary,
+                      fontSize: 15,
+                      fontWeight: '600',
+                      paddingBottom: 10,
+                    }}>
+                    {errors.confPass}
+                  </Text>
+                </View>
+              )}
+              <CustomInput
+                text="Enter Phone no."
+                iconName="call"
+                customStyle={styles.custom}
+                keyboardType={'numeric'}
+                onChangeText={handleChange('phone')}
+              />
+              {errors.phone && touched.phone && (
+                <View>
+                  <Text
+                    style={{
+                      color: COLORS.primary,
+                      fontSize: 15,
+                      fontWeight: '600',
+                      paddingBottom: 10,
+                    }}>
+                    {errors.phone}
+                  </Text>
+                </View>
+              )}
+              <CustomInput
+                text="Enter your Address"
+                iconName="location-sharp"
+                customStyle={{marginBottom: 15}}
+                onChangeText={handleChange('address')}
+              />
+              {errors.address && touched.address && (
+                <View>
+                  <Text
+                    style={{
+                      color: COLORS.primary,
+                      fontSize: 15,
+                      fontWeight: '600',
+                      paddingBottom: 10,
+                    }}>
+                    {errors.address}
+                  </Text>
+                </View>
+              )}
+              <Buttons
+                text={'REGISTER'}
+                inAppIcon={'account-plus'}
+                iconName="angle-right"
+                onPress={handleSubmit}
+              />
+            </>
+          )}
+        </Formik>
       </ScrollView>
     </>
   );
@@ -131,10 +263,10 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.white,
     justifyContent: 'center',
     alignItems: 'center',
-    paddingBottom: 200,
+    paddingBottom: 50,
   },
   custom: {
-    marginBottom: 20,
+    marginBottom: 10,
   },
   icon: {
     marginRight: 18,
@@ -154,12 +286,12 @@ const styles = StyleSheet.create({
   },
   modalStyle: {
     paddingTop: 15,
-    height: DIM.height * 0.55,
-    width: DIM.width * 0.9,
+    height: DIM.height * 0.58,
+    width: DIM.width * 0.95,
     backgroundColor: 'white',
     top: '25%',
     bottom: '25%',
-    left: '5%',
+    left: '3%',
     right: '5%',
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
@@ -181,7 +313,7 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.slate,
     borderRadius: 15,
     paddingLeft: 20,
-    marginBottom: 20,
+    marginBottom: 10,
   },
   text: {
     fontSize: 17,
