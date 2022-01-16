b3b23e1c9170a2a9b9eef80b4d45a2e38e9eb3cf
Faiaz Rahman <faiazrahman70@gmail.com>
2022-01-16 12:51:56 +0600

    Stack Navigator changed with Drawer Navigator

--- src/navigation/HomeNavigator.js
+++ src/navigation/HomeNavigator.js
@@ -1,22 +1,74 @@
 import React from 'react';
 
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Home from '../screens/Home';
+import Payment from '../screens/Payment';
+import Logout from '../screens/Logout';
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 const HomeNavigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen
+    <Drawer.Navigator
+      initialRouteName="home"
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Drawer.Screen
         name="home"
         component={Home}
         options={{
-          headerShown: false,
+          drawerIcon: () => {
+            return <FontAwesome name="home" size={30} color={'slategrey'} />;
+          },
+          drawerLabelStyle: {
+            fontWeight: '900',
+            fontSize: 18,
+            marginLeft: 7,
+          },
+          drawerLabel: 'Home',
         }}
       />
-    </Stack.Navigator>
+      <Drawer.Screen
+        name="payment"
+        component={Payment}
+        options={{
+          drawerIcon: () => {
+            return (
+              <FontAwesome
+                name="credit-card-alt"
+                size={20}
+                color={'slategrey'}
+              />
+            );
+          },
+          drawerLabelStyle: {
+            fontWeight: '900',
+            fontSize: 18,
+            marginLeft: 7,
+          },
+          drawerLabel: 'Payment',
+        }}
+      />
+      <Drawer.Screen
+        name="logout"
+        component={Logout}
+        options={{
+          drawerIcon: () => {
+            return <Ionicons name="log-out" size={35} color={'slategrey'} />;
+          },
+          drawerLabelStyle: {
+            fontWeight: '900',
+            fontSize: 18,
+          },
+          drawerLabel: 'Logout',
+        }}
+      />
+    </Drawer.Navigator>
   );
 };
 
