922cd98b8fd883942c60b7e4b22fc0a6426857db
Faiaz Rahman <faiazrahman70@gmail.com>
2022-01-21 00:45:00 +0600

    Data fetch from Firebase added & Show details option added

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,5 @@
 import {useNavigationState} from '@react-navigation/native';
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import {
   StyleSheet,
   Text,
@@ -8,6 +8,10 @@ import {
   ScrollView,
   TextInput,
   StatusBar,
+  RefreshControl,
+  ActivityIndicator,
+  Modal,
+  Image,
 } from 'react-native';
 
 import firestore from '@react-native-firebase/firestore';
@@ -16,10 +20,14 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Octicons from 'react-native-vector-icons/Octicons';
 import Card from '../components/Card';
 
-import {COLORS, DIM, Data} from '../constants';
+import {COLORS, DIM} from '../constants';
 
 export default function Home({navigation}) {
   let state = useNavigationState(state => state.index);
+
+  const wait = timeout => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+  };
 
   useEffect(() => {
     return () => {
@@ -27,66 +35,78 @@ export default function Home({navigation}) {
     };
   }, [state]);
 
-  const [searchBoxVisible, setSearchBoxVisible] = useState(false);
-  const [posts, setPosts] = useState();
+  useEffect(() => {
+    fetchPosts();
+  }, [posts]);
 
-  const list = [];
+  const [searchBoxVisible, setSearchBoxVisible] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [showDetails, setShowDetails] = useState(false);
+  const [item, setItem] = useState();
 
   const fetchPosts = async () => {
+    const list = [];
     try {
-      await firestore().collection('owners').orderBy('createdAt', 'desc').get();
-      // .then(querySnapshot => {
-      //   querySnapshot.forEach(element => {
-      //     const {
-      //       userId,
-      //       name,
-      //       createdAt,
-      //       address,
-      //       email,
-      //       state,
-      //       city,
-      //       szPool,
-      //       len,
-      //       brdth,
-      //       mob,
-      //       image,
-      //       pan,
-      //       gst,
-      //       selectedDays,
-      //     } = doc.data();
+      await firestore()
+        .collection('owners')
+        .get()
+        .then(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            const {
+              name,
+              address,
+              email,
+              pass,
+              state,
+              city,
+              szPool,
+              len,
+              brdth,
+              image,
+              mob,
+              pan,
+              gst,
+              selectedDays,
+            } = doc.data();
+            list.push({
+              id: doc.id,
+              name,
+              address,
+              email,
+              pass,
+              state,
+              city,
+              szPool,
+              len,
+              brdth,
+              image,
+              mob,
+              pan,
+              gst,
+              selectedDays,
+            });
+          });
+        });
+      setPosts(list);
 
-      //     list.push({
-      //       id: doc.id,
-      //       userId,
-      //       name,
-      //       createdAt,
-      //       address,
-      //       email,
-      //       state,
-      //       city,
-      //       szPool,
-      //       len,
-      //       brdth,
-      //       mob,
-      //       image,
-      //       pan,
-      //       gst,
-      //       selectedDays,
-      //     });
-      //     setPosts(list);
-      // });
-      // });
-      console.log('Total post', querySnapshot.size);
-    } catch (e) {
-      console.log(e);
+      if (loading) setLoading(false);
+    } catch (error) {
+      console.log(error);
     }
   };
 
-  fetchPosts();
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    fetchPosts();
+    wait(3500).then(() => setRefreshing(false));
+  }, []);
 
   return (
     <View style={styles.container}>
       <StatusBar barStyle="dark-content" backgroundColor={COLORS.white} />
+
       <View style={styles.header}>
         <TouchableOpacity
           onPress={() => navigation.toggleDrawer()}
@@ -116,17 +136,126 @@ export default function Home({navigation}) {
           <FontAwesome name="search" size={30} color={COLORS.black} />
         </TouchableOpacity>
       </View>
+      {loading && (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        </View>
+      )}
+      {!loading && (
+        <>
+          {showDetails && (
+            <Modal
+              animationType="slide"
+              transparent
+              onRequestClose={() => {
+                // console.log('Modal Closed.');
+                setShowDetails(false);
+              }}>
+              <View style={styles.modalStyle}>
+                <ScrollView
+                  contentContainerStyle={{
+                    paddingBottom: 100,
+                  }}>
+                  <View style={styles.modalHeaderContainer}>
+                    <Image
+                      source={{uri: item.image}}
+                      style={styles.modalImageStyle}
+                    />
+                    <View
+                      style={{
+                        // backgroundColor: 'green',
+                        flex: 1,
+                        paddingLeft: 15,
+                      }}>
+                      <Text style={styles.titleText}>Owner :</Text>
+                      <Text style={styles.subTitleText}>{item.name}</Text>
 
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{
-          paddingBottom: 40,
-          paddingTop: 10,
-        }}>
-        {list.map((item, index) => {
-          return <Card key={index} item={item} />;
-        })}
-      </ScrollView>
+                      <Text style={styles.titleText}>Location :</Text>
+                      <Text style={styles.subTitleText}>{item.address}</Text>
+                    </View>
+                  </View>
+                  <View
+                    style={{
+                      flex: 1,
+                      marginTop: 10,
+                      marginLeft: 15,
+                    }}>
+                    <Text style={styles.underSubtext}>
+                      {'Breadth of Pool: ' + item.brdth}
+                    </Text>
+                    <Text style={[styles.underSubtext, styles.custom]}>
+                      {'Length of the Pool: ' + item.len}
+                    </Text>
+                    <Text style={[styles.underSubtext, styles.custom]}>
+                      {'Contact Number: ' + item.mob}
+                    </Text>
+                    <Text style={[styles.underSubtext, styles.custom]}>
+                      {'Available Days: '}
+                    </Text>
+                    {item.selectedDays.map((i, index) => {
+                      return (
+                        <View
+                          style={{
+                            flex: 1,
+                            flexDirection: 'row',
+                            // backgroundColor: 'green',
+                            height: '10%',
+                            width: '100%',
+                            justifyContent: 'space-evenly',
+                          }}
+                          key={index}>
+                          <Text
+                            style={{
+                              fontSize: 22,
+                              color: 'grey',
+                              fontWeight: '600',
+                            }}>
+                            {i}
+                          </Text>
+                        </View>
+                      );
+                    })}
+                    <Text style={[styles.underSubtext, {marginTop: 25}]}>
+                      {'Size of the Pool: ' + item.szPool}
+                    </Text>
+                  </View>
+                </ScrollView>
+              </View>
+            </Modal>
+          )}
+          <ScrollView
+            refreshControl={
+              <RefreshControl
+                refreshing={refreshing}
+                onRefresh={onRefresh}
+                colors={[COLORS.basic, 'blue', 'red', 'green']}
+              />
+            }
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{
+              paddingBottom: 40,
+              paddingTop: 10,
+            }}>
+            {posts.map((item, index) => {
+              return (
+                <Card
+                  key={index}
+                  item={item}
+                  onPress={() => {
+                    setShowDetails(true);
+                    setItem(item);
+                  }}
+                />
+              );
+            })}
+          </ScrollView>
+        </>
+      )}
     </View>
   );
 }
@@ -137,6 +266,9 @@ const styles = StyleSheet.create({
     paddingTop: DIM.height * 0.01,
     alignItems: 'center',
     backgroundColor: COLORS.white,
+  },
+  custom: {
+    marginTop: 7,
   },
   header: {
     height: DIM.height * 0.1,
@@ -158,6 +290,39 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 30,
     backgroundColor: COLORS.slate,
+  },
+  modalHeaderContainer: {
+    height: '45%',
+    width: '100%',
+    paddingLeft: 15,
+    // backgroundColor: 'purple',
+    flexDirection: 'row',
+  },
+  modalStyle: {
+    paddingTop: 15,
+    height: DIM.height * 0.58,
+    width: DIM.width * 0.95,
+    backgroundColor: 'white',
+    top: '25%',
+    bottom: '25%',
+    left: '3%',
+    right: '5%',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    borderBottomLeftRadius: 25,
+    borderBottomRightRadius: 25,
+    borderRightColor: 'green',
+    borderLeftColor: COLORS.lightViolet,
+    borderTopColor: 'tomato',
+    borderBottomColor: 'yellow',
+    borderWidth: 10,
+    opacity: 0.75,
+    elevation: 4,
+  },
+  modalImageStyle: {
+    height: '100%',
+    width: '50%',
+    borderRadius: 10,
   },
   searchStyle: {
     width: '20%',
@@ -183,5 +348,24 @@ const styles = StyleSheet.create({
     height: '100%',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  titleText: {
+    marginTop: 10,
+    fontSize: 23,
+    color: '#2F4F4F',
+    fontWeight: '800',
+    letterSpacing: 2,
+  },
+  subTitleText: {
+    fontSize: 17,
+    color: '#2F4F4F',
+    fontWeight: '600',
+    letterSpacing: 1.2,
+  },
+  underSubtext: {
+    fontSize: 18,
+    color: '#2F4F4F',
+    fontWeight: '800',
+    letterSpacing: 2,
   },
 });
